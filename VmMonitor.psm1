1e9690f525408fc20f7bff5c52180dc3a3b6f6db
Janah, Anass <janah.anass@courrier.uqam.ca>
2022-03-10 18:26:38 -0500

    code refactoring and fixing the quite menu bug

--- VmMonitor.psm1
+++ VmMonitor.psm1
@@ -5,17 +5,18 @@
 	VmMonitor module : ce module est dans le cadre du devoir 1 du cours CR430.
 					   Il sert à survailler des machines virtuelles Azure
 #>
+
 function Show-Menu {
 	param (
-		[string]$Title = 'Survaillance des VMs Azure - Menu principal'
+		[string]$Title = 'Surveillance des VMs Azure - Menu principal'
 	)
 	Clear-Host
-	Write-Host "================ $Title ================" -ForegroundColor blue
+	Write-Host "================ $Title ================"
     
-	Write-Host "1: Afficher la consomation du CPU d'une VM." -ForegroundColor DarkMagenta
-	Write-Host "2: Demmarer toutes les VMs eteintes" -ForegroundColor DarkMagenta
-	Write-Host "3: Verifier Etat Antimalware VM Windows" -ForegroundColor DarkMagenta
-	Write-Host "Q: Quitter." -ForegroundColor DarkMagenta
+	Write-Host "1: Afficher la consomation du CPU d'une VM."
+	Write-Host "2: Demmarer toutes les VMs eteintes"
+	Write-Host "3: Verifier Etat Antimalware VM Windows"
+	Write-Host "Q: Quitter."
 }
 
 function Show-CPUsageVmAz {
@@ -24,7 +25,7 @@ function Show-CPUsageVmAz {
 	
 	# cacher les avertissements
 	$warningPreference = "SilentlyContinue"
-	$vmname = Read-Host "Entrez le nom de la VM parmi ceux listees plus haut: "
+	$vmname = Read-Host -Prompt "Entrez le nom de la VM parmi ceux listees plus haut: "
 	$vmaz = Get-AzVM -VMName $vmname
 	$AverageCPU = ((Get-AzMetric -ResourceId $vmaz.id -TimeGrain 00:01:00 -DetailedOutput -MetricNames "Percentage CPU").Data.Average | Measure-Object -Average).Average
 	$formatCpuPerc = "{0:P}" -f $AverageCPU
@@ -37,8 +38,6 @@ function Show-CPUsageVmAz {
 		Write-Host "Le pourcentage d'utilisation CPU durant la derniere heure est : " $formatCpuPerc -ForegroundColor DarkYellow
 	}
 	
-	pause
-	Start-Menu
 }
 
 function Start-VmAz {
@@ -47,18 +46,15 @@ function Start-VmAz {
 	foreach ($vm in $vmList) {
 		$vmName = $vm.Name
 		if ($vm.PowerState -ne 'VM running') {
-			Write-Host "Demarage de $vmName ..." -ForegroundColor darkblue
+			Write-Host "Demarage de $vmName ..." 
 			Start-AzureRmVM -Name $vm.Name -ResourceGroupName $vm.ResourceGroupName
-			Write-Host "$vmName est demarree maintenant!"  -ForegroundColor darkgreen
+			Write-Host "$vmName est demarree maintenant!" 
 		}
 		else {
-			Write-Host "$vmName est deja demarree!"  -ForegroundColor darkgreen
+			Write-Host "$vmName est deja demarree!"
 		}
 	}
 	
-	# attendre le signale du user avant de revenir au menu
-	pause
-	Start-Menu
 }
 
 function CheckAntMalWinAzVm {
@@ -71,43 +67,56 @@ function CheckAntMalWinAzVm {
     
 		[string]$ResourceGroupName
 	
-
 	)
-	$Config_String = [IO.File]::ReadAllText('c:\AV.json')
+		
 	$VMs = Get-AzureRmVM -ResourceGroupName $ResourceGroupName
-	Write-Host $($VMs| Format-Table| Out-String ) -ForegroundColor Blue
-
-	foreach ($vm in $vms) {
+						
+	foreach ($vm in $VMs) {
 		$vmOs = $vm.StorageProfile.OsDisk.OsType
-		if ( $vmOs -eq "Windows") {
-			$avExtension = Get-AzureRmVMExtension -ResourceGroupName $ResourceGroupName -VMName $vm.name -Name IaaSAntimalware
-			$publicSettings = ConvertFrom-Json $avExtension.PublicSettings
-			#$avExtension.PublicSettings["AntimalwareEnabled"]
-			$publicSettings
-				
-			if ($avExtension.PublicSettings["AntimalwareEnabled"] -eq "False") {
-				Write-Host "Installation du antiMalware pour la VM $vm.name" -ForegroundColor DarkYellow
-				Set-AzureVMMicrosoftAntimalwareExtension -AntimalwareConfiguration $Config_String | Update-AzureVM
+		if ( $vmOs -eq "Windows") {				
+			try {						
+				$ErrorActionPreference = 'silentlycontinue'
+				$avExtension = Get-AzureRmVMExtension -ResourceGroupName $ResourceGroupName -VMName $vm.name -Name "IaaSAntimalware"					
+					
+				if ($null -eq $avExtension) {
+					write-host "IaaSAntimalware not Installed on " $vm.name
+							
+					$location = " Canada Central "
+					$typeHandlerVer = "1.3"
+					
+					write-Host "Installing Antimalware on " $vm.name " ......." 
+					Set-AzureRmVMExtension -ResourceGroupName $resourceGroupName -VMName $vm.Name -Name "IaaSAntimalware" -Publisher "Microsoft.Azure.Security" -ExtensionType "IaaSAntimalware" -typeHandlerVer $typeHandlerVer -location $location
+					write-host "IaaSAntimalware Successfuly Installed "
+				}
+				Else {
+					write-host "Existing" $avExtension.name" Already Installed on" $vm.name -ForegroundColor Green
+				}					
+						
+						
 			}
+				
+			catch {
+				Write-Warning $_
+			}
+			
 		}
 		else {
-			Write-Host "Le SE de la VM $($vm.name) est $vmOs. Cette fonction est uniquement pour les VM Windows" -ForegroundColor Darkred
-		}
-	}
-	pause
-	Start-Menu
+			Write-Host "Le SE de la VM $($vm.name) est $vmOs. Cette fonction est uniquement pour les VM Windows"
+		} 			
+	}				
 }
 
 
 function Start-Menu {
 	do {
 		Show-Menu
-		$selection = Read-Host "Entrez votre choix: "
+		$selection = Read-Host -Prompt "Entrez votre choix: "
 		try {
 			switch ($selection) {
 				'1' {
 					Write-Host "vous avez choisi option #1" -ForegroundColor green
 					Show-CPUsageVmAz
+
 				} '2' {
 					Write-Host "vous avez choisi option #2" -ForegroundColor green
 					Start-VmAz
@@ -115,6 +124,7 @@ function Start-Menu {
 					Write-Host "vous avez choisi option #3" -ForegroundColor green
 					CheckAntMalWinAzVm
 				} 
+				 
 			}
 		}
 		catch {
